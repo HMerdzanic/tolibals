ff4030ad3a3dcd923553f2fb43e29d6717260d20
realFleks <realFleks@users.noreply.github.com>
2017-11-16 18:17:26 +0100

    Add files via upload

---
+++ java/com/example/hacking/tolibals10/LoseDraw.java
@@ -0,0 +1,14 @@
+package com.example.hacking.tolibals10;
+
+import android.content.Context;
+import android.view.View;
+
+
+
+public class LoseDraw extends View {
+
+
+    public LoseDraw(Context context) {
+        super(context);
+    }
+}
